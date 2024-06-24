c386a39daab74a3fc526ca4a49d219cc556bfb23
Yogitabhargava <yogitab@indiratrade.com>
2024-06-24 17:55:55 +0530

    created structure

---
+++ src/app/components/CardList/CardList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Pagination from '../Pagination/Pagination'
+const CardList = () => {
+  return (
+    <div>
+      <h1>Hello from cardlist</h1>
+      <pagination/>
+    </div>
+  )
+}
+
+export default CardList
---
+++ src/app/components/ThemeToggle/ThemeToggle.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import style from "./themeToggle.module.css"
+import Image from 'next/image'
+const ThemeToggle = () => {
+  return (
+    <div className={style.container}>
+  <Image src="/moon.png" alt="moon" width={14} height={14}/>
+<div className={style.ball}></div>
+  <Image src="/sun.png" alt="sun"  width={14} height={14}/>
+ 
+    </div>
+  )
+}
+
+export default ThemeToggle;

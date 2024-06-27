b022e6b47a9295c8eba86b21796d3bb1c7e6bab3
Yogitabhargava <yogitab@indiratrade.com>
2024-06-27 16:02:47 +0530

    corrected prisma

--- src/app/components/AuthLinks/AuthLinks.jsx
+++ src/app/components/AuthLinks/AuthLinks.jsx
@@ -11,7 +11,7 @@ const AuthLinks = () => {
 
   return (
     <>
-      {status === "unauthenticated" ? (
+      {status === "authenticated" ? (
         <Link href="/login" className={styles.link}>
           Login
         </Link>
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,12 +1,12 @@
 import GoogleProvider from "next-auth/providers/google"
 import { PrismaAdapter } from "@auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
+// import { PrismaClient } from "@prisma/client"
 import NextAuth from "next-auth"
 
-const prisma = new PrismaClient()
+// const prisma = new PrismaClient()
 
 export const authOptions = {
-  adapter: PrismaAdapter(prisma),
+  // adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
